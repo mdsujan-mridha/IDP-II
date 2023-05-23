5e08870f1f016cb1d2543a439a3d08353dc2ef45
mdsujan-mriddha <mdsujan134523@gmail.com>
2023-05-23 21:36:37 +0600

    create redux functionality on front-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,30 @@
 
+import { useEffect } from 'react';
 import { Route,  Routes } from 'react-router-dom';
+import WebFont from 'webfontloader';
 import './App.css';
 import Doctors from './Components/Doctors/Doctors';
 import Home from './Components/Home/Home';
 import SingleDoctor from './Components/SingleDoctor/SingleDoctor';
 import Footer from './Share/Footer/Footer';
 import Navbar from './Share/Navbar/Navbar';
+// import store from './store';
 
 function App() {
+
+   useEffect( () =>{
+     WebFont.load({
+      google:{
+        families:["Roboto Condensed","Roboto","Droid Sans"],
+      },
+     });
+    
+    //  store.dispatch()
+
+   },[]);
+
+
+
   return (
     <>
       <Navbar />
---
+++ front-end/src/actions/userAction.js
@@ -0,0 +1,36 @@
+
+
+// login user 
+
+import axios from "axios";
+import { LOGIN_FAIL, LOGIN_REQUEST, LOGIN_SUCCESS, REGISTER_USER_FAIL, REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS } from "../constant/userConstant"
+
+
+export const login =(email,password)=>async(dispatch)=>{
+     try{
+         
+        dispatch({type:LOGIN_REQUEST});
+
+        const config ={Header:{"Content-type":"application/json"}}
+        const {data } = await axios.post(`http://localhost:8000/api/v1/login`,{email,password},config)
+        dispatch({type:LOGIN_SUCCESS,payload:data.user})
+
+     }catch(error){
+        dispatch({type:LOGIN_FAIL,payload:error.response.data.message})
+     }
+}
+
+// register user 
+export const register = (userData)=>async(dispatch)=>{
+    try{
+     
+        dispatch({type:REGISTER_USER_REQUEST});
+        const config = {Headers:{"Content-type":"multipart/from-data"}};
+        const data = await axios.post(`http://localhost:8000/api/v1/register`,userData,config);
+        dispatch({type:REGISTER_USER_SUCCESS,payload:data.user});
+
+    }catch(error){
+        dispatch({type:REGISTER_USER_FAIL,payload:error.response.data.message})
+    }
+
+}
